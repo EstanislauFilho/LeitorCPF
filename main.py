732d18356f7fa23aa273af58606b1e8094c4e7a8
EstanislauFilho <estanislau.sena@gmail.com>
2021-03-21 21:09:25 -0300

    Classe principal.

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 20:48:24 2021
+
+@author: estanislau
+"""
+
+import cv2
+import glob
+
+class LeitorCPF():
+    
+    def __init__(self):
+        pass
+    
+    def main(self):
+        pass
+    
+if __name__ == '__main__':
+    app = LeitorCPF()
+    app.main()
